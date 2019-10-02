7079da6f82bde2b65830f33625c106e2fe7141fa
adityasunny1189 <adityapathak1189@gmail.com>
2019-10-02 22:41:20 +0530

    js file

--- index.js
+++ index.js
@@ -1,78 +1,80 @@
-function flame(z) 
-{
-  switch(z)
-    {
-        case 1:document.querySelector('#friends').style.display = 'flex';
-                // document.querySelector('.close').addEventListener('click',
-                // function() {
-                //     document.querySelector('.first').style.display = 'none';
-                // });
-            break;
+// document.querySelector(".func").addEventListener("click" , a_new_func);
 
-        case 2:document.querySelector('#love').style.display = 'flex';
-            //    document.querySelector('.close').addEventListener('click',
-            //     function() {
-            //         document.querySelector('.first').style.display = 'none';
-            //     });
-             break;
+// function flame(z) 
+// {
+//   switch(z)
+//     {
+//         case 1:document.querySelector('#friends').style.display = 'flex';
+//                 document.querySelector('.close').addEventListener('click',
+//                 function() {
+//                     document.querySelector('.first').style.display = 'none';
+//                 });
+//             break;
 
-        case 3:document.querySelector('#affection').style.display = 'flex';
-                // document.querySelector('.close').addEventListener('click',
-                // function() {
-                //     document.querySelector('.first').style.display = 'none';
-                // });
-            break;
+//         case 2:document.querySelector('#love').style.display = 'flex';
+//                document.querySelector('.close').addEventListener('click',
+//                 function() {
+//                     document.querySelector('.first').style.display = 'none';
+//                 });
+//              break;
 
-        case 4:document.querySelector('#weeding').style.display = 'flex';
-                // document.querySelector('.close').addEventListener('click',
-                // function() {
-                //     document.querySelector('.first').style.display = 'none';
-                // });
-            break;
+//         case 3:document.querySelector('#affection').style.display = 'flex';
+//                 document.querySelector('.close').addEventListener('click',
+//                 function() {
+//                     document.querySelector('.first').style.display = 'none';
+//                 });
+//             break;
 
-        case 5:document.querySelector('#enemy').style.display = 'flex';
-                // document.querySelector('.close').addEventListener('click',
-                // function() {
-                //     document.querySelector('.first').style.display = 'none';
-                // });
-            break;
+//         case 4:document.querySelector('#weeding').style.display = 'flex';
+//                 document.querySelector('.close').addEventListener('click',
+//                 function() {
+//                     document.querySelector('.first').style.display = 'none';
+//                 });
+//             break;
 
-        default:alert("No Relation\n");
-    }
-}
+//         case 5:document.querySelector('#enemy').style.display = 'flex';
+//                 document.querySelector('.close').addEventListener('click',
+//                 function() {
+//                     document.querySelector('.first').style.display = 'none';
+//                 });
+//             break;
 
-function a_new_func() 
-{
-    var name1 = document.getElementById('Name1').value;
-    var name2 = document.getElementById('Name2').value;
+//         default:alert("No Relation\n");
+//     }
+// }
+
+// function a_new_func() 
+// {
+//     var name1 = document.getElementById('Name1').value;
+//     var name2 = document.getElementById('Name2').value;
     
-    var x = name1.length;
-    var y = name2.length;
-    var z = x + y;
-    var i = 0;
-    var j = 0;
-    for(i = 0 ; i < x ; i++)
-    {
-        for(j = 0 ; j < y ; j++)
-        {
-            if(name1[i] === name2[j])
-            {
-                name1[i] = name2[j] = ' ';
-                z = z - 2;
-            }
-        }
-    }
-    if(z <= 5)
-    {
-        flame(z);
-    }
+//     var x = name1.length;
+//     var y = name2.length;
+//     var z = x + y;
+//     var i = 0;
+//     var j = 0;
+//     for(i = 0 ; i < x ; i++)
+//     {
+//         for(j = 0 ; j < y ; j++)
+//         {
+//             if(name1[i] === name2[j])
+//             {
+//                 name1[i] = name2[j] = ' ';
+//                 z = z - 2;
+//             }
+//         }
+//     }
+//     if(z <= 5)
+//     {
+//         flame(z);
+//     }
     
-    else if(z > 5)
-    {
-        r = z % 5;
-        flame(r);
-    }
-}
+//     else if(z > 5)
+//     {
+//         z = z % 5;
+//         flame(z);
+//     }
+// }
 
 
 
@@ -97,3 +99,77 @@ function a_new_func()
 //         console.log(a);
 //     }
 // }
+
+var name1 = document.getElementById('Name1').value;
+var name2 = document.getElementById('Name2').value;
+
+var x = name1.length;
+var y = name2.length;
+var z = x + y;
+var i = 0;
+var j = 0;
+for(i = 0 ; i < x ; i++)
+{
+    for(j = 0 ; j < y ; j++)
+    {
+        if(name1[i] === name2[j])
+        {
+            name1[i] = name2[j] = ' ';
+            z = z - 2;
+        }
+    }
+}
+if(z <= 5)
+    {
+        z = z;
+    }
+    
+else if(z > 5)
+    {
+        z = z % 5;
+    }
+for(var i = 0 ; i < z ; i++)
+{
+    document.querySelector(".func").addEventListener("click" , function () 
+    {
+        switch(z)
+        {
+            case 1:document.querySelector('#friends').style.display = 'flex';
+                    document.querySelector('.close').addEventListener('click',
+                    function() {
+                        document.querySelector('.first').style.display = 'none';
+                    });
+                break;
+
+            case 2:document.querySelector('#love').style.display = 'flex';
+                document.querySelector('.close').addEventListener('click',
+                    function() {
+                        document.querySelector('.first').style.display = 'none';
+                    });
+                break;
+
+            case 3:document.querySelector('#affection').style.display = 'flex';
+                    document.querySelector('.close').addEventListener('click',
+                    function() {
+                        document.querySelector('.first').style.display = 'none';
+                    });
+                break;
+
+            case 4:document.querySelector('#weeding').style.display = 'flex';
+                    document.querySelector('.close').addEventListener('click',
+                    function() {
+                        document.querySelector('.first').style.display = 'none';
+                    });
+                break;
+
+            case 5:document.querySelector('#enemy').style.display = 'flex';
+                    document.querySelector('.close').addEventListener('click',
+                    function() {
+                        document.querySelector('.first').style.display = 'none';
+                    });
+                break;
+
+            default:alert("No Relation\n");
+        }
+    });
+}
